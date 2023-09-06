4eacab97c4590d0c8a1a09ce8d8500356b944647
YamtalDev <tal.aharon.work@gmail.com>
2023-09-06 08:50:55 +0300

    Commiting last changes in repo 💭️🚀💭️

--- system_programing/include/libraries.h
+++
@@ -1,7 +0,0 @@
-#ifndef __LIBRARIES_H__
-#define __LIBRARIES_H__
-
-/* Complexities - time Complexity O(1), space complexity O(1). */
-void Foo(int);
-
-#endif /* __LIBRARIES_H__ */
--- system_programing/include/memory_mapping_full.h
+++
@@ -1,8 +0,0 @@
-#ifndef __MEMORY_MAPPING_FULL_H__
-#define __MEMORY_MAPPING_FULL_H__
-
-/* Complexities - time Complexity O(1), space complexity O(1). */
-int Mapping(int argc, char **argv, char **env);
-/* Complexities - time Complexity O(1), space complexity O(1). */
-
-#endif /* __MEMORY_MAPPING_FULL_H__ */
